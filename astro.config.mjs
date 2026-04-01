cf88e7da0d3f398511f363880d3179765703104c
farhat60 <abdullahfarhat.60@gmail.com>
2026-04-01 17:42:35 -0400

    web routing fix attempt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  site: "https://farhat60.github.io",
+  base: "/portfolio",
   output: "static",
+  build: {
+    inlineStylesheets: "always",
+  },
 });
