707ec0b143c7f49bed96363ff543f61bc66448ba
farhat60 <abdullahfarhat.60@gmail.com>
2026-05-28 15:22:28 -0400

    fix: base navigation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://afarhat.org",
-  base: "/",
   output: "static",
   build: {
     inlineStylesheets: "always",
